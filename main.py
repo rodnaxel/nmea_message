5a427cdcda464b6ff680547849a8be35661675d7
Aleksandr Smirnov <alvonrims@gmail.com>
2019-05-16 19:47:50 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,359 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os.path
+import sys
+from collections import ChainMap
+
+import logging
+
+from PyQt5 import QtCore
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+import app_rc
+
+import model
+
+__title__ = "Генератор сообщений NMEA-0183"
+__version__ = "0.1.0"
+__author__ = "Александр Смирнов"    
+
+
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+    handlers=[
+        #logging.FileHandler("{0}/{1}.log".format(logPath, fileName)),
+        logging.StreamHandler()
+    ])
+logger = logging.getLogger()
+
+
+class Ui(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.timer_id = 0
+        self.isBlink = False
+        self.status = {}
+
+        self.createUI()
+
+        self.settings = dict(ChainMap(self.portbox_data, self.message_data))
+
+        self.transmitter = model.Transmitter()
+
+    def _center(self):
+        """ This method aligned main window related center screen """
+        frameGm = self.frameGeometry()
+        screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
+        centerPoint = QApplication.desktop().screenGeometry(screen).center()
+        frameGm.moveCenter(centerPoint)
+        self.move(frameGm.topLeft())
+
+    def createUI(self):
+        self.setWindowTitle("{0} (версия {1})".format(__title__, __version__))
+        self.setWindowIcon(QIcon(":/rc/logo.png"))
+        self.setMaximumSize(800, 500)
+        
+        centralWgt = QWidget(self)
+        self.setCentralWidget(centralWgt)
+
+
+        # Create widgets
+
+        self.control = self.createButtons()
+        self.portbox = self.createPortbox()
+
+        message_group = QGroupBox("Настройка сообщения", self)
+        self.messagebox = self.createMessageBox()
+
+        term_wgt = self.createTerminal()
+
+        self.createStatusbar()
+
+        # Layouts
+        centralLayout = QVBoxLayout(centralWgt)
+        centralLayout.addWidget(self.control)
+        settingsLayout = QHBoxLayout()
+        settingsLayout.addWidget(self.portbox)
+
+        message_layout = QHBoxLayout(message_group)
+        message_layout.addWidget(self.messagebox)
+
+        settingsLayout.addWidget(message_group)
+        centralLayout.addLayout(settingsLayout)
+        centralLayout.addWidget(term_wgt)
+
+        self._center()
+
+        self.show()
+
+    def createTerminal(self):
+        term_wgt = QWidget()
+
+        self.terminal = terminal_output = QTextEdit()
+        terminal_output.setReadOnly(True)
+
+        def _clear():
+            self.terminal.clear()
+
+        btn_clear = QPushButton('Очистить')
+        btn_clear.setFixedWidth(60)
+
+        btn_clear.clicked.connect(_clear)
+
+        term_layout = QVBoxLayout(term_wgt)
+        term_layout.addWidget(self.terminal)
+        term_layout.addWidget(btn_clear)
+
+        return term_wgt
+
+    def createButtons(self):
+        wgt = QWidget()
+        layout = QHBoxLayout(wgt)
+
+        self.buttons = {}
+        for (name, key, enabled, action, icon) in (
+                ('старт', 'start', True, self._on_start, ':/rc/red-start.png'),
+                ('стоп', 'stop',  False, self._on_stop, ':/rc/red-stop.png'),
+                ('выход', 'exit', True, self._on_quit, ':/rc/red-quit.png')
+        ):
+            button = QPushButton(name.capitalize())
+            button.setEnabled(enabled)
+            button.setFixedSize(80, 25)
+            
+            if icon: 
+                button.setIcon(QIcon(icon))
+            
+            button.setStyleSheet("text-align: left")
+            
+            if action:
+                button.clicked.connect(action)
+
+            layout.addWidget(button)
+            layout.setSpacing(1)
+
+            self.buttons[key] = button
+        return wgt
+
+    def createPortbox(self):
+        """ Port configuration"""
+        wgt = QGroupBox('Настройки порта', self)
+        layout = QGridLayout(wgt)
+        layout.setSpacing(1)
+
+        # Slots
+        def _update_data():
+            self.portbox_data = {"port": wgt.findChild(QComboBox, "port").currentText(),
+                                 "baudrate": int(wgt.findChild(QComboBox, "baudrate").currentText())
+                                 }
+
+        def _on_find_ports():
+            port = wgt.findChild(QComboBox, "port")
+            port.clear()
+            port.addItems(model.serial_ports())
+            if not port.isEnabled():
+                port.setEnabled(True)
+
+        row = 0
+        for key, name, items in (
+            ('port', 'Порт', available_ports),
+            ('baudrate', 'скорость', ['4800', '9600', '19200', '38400', '57600', '115200']),
+            ('bytesize', 'биты данных', ['5', '6', '7', '8']),
+            ('parity', 'четность', ['N']),
+            ('stopbits', 'стоп', ['1', '1.5', '2'])
+        ):
+            combo = QComboBox()
+            combo.setObjectName(key)
+            combo.setFixedWidth(60)
+
+            if items:
+                combo.addItems(items)
+            else:
+                combo.setDisabled(True)
+
+            combo.currentTextChanged['QString'].connect(_update_data)
+
+            layout.addWidget(QLabel(f"{name.capitalize()}:"), row, 0)
+            layout.addWidget(combo, row, 1)
+            row += 1
+
+        # Button re-find available port
+        btnRescan = QPushButton("Обновить")
+        btnRescan.setFixedWidth(60)
+        layout.addWidget(btnRescan, 0, 2)
+
+        # Connect signal/slot
+        btnRescan.clicked.connect(_on_find_ports)
+
+        _update_data()
+
+        return wgt
+
+    def createMessageBox(self):
+        """ Create message configuration"""
+        wgt = QWidget()
+        layout = QGridLayout(wgt)
+
+        # Slots
+        def _update_data():
+            self.message_data = {
+                'id': wgt.findChild(QComboBox, "id").currentText(),
+                'interval': wgt.findChild(QComboBox, "interval").currentText(),
+                'depth': round(wgt.findChild(QDoubleSpinBox, "depth").value(), 2),
+                'danger': wgt.findChild(QSpinBox, "danger").value(),
+                'accuracy': wgt.findChild(QComboBox, "accuracy").currentText()
+            }
+
+        row = 0
+        for key, name, items in (
+            ('id', 'индефикатор', ['SDDBT']),
+            ('interval', 'темп, мс', ['100', '500', '1000', '2000']),
+            ('depth', 'глубина, М', (0, 9999.9)),
+            ('danger', 'Оп. глубина, М', (0, 99)),
+            ('accuracy', 'Исправность', ('V', 'A'))
+        ):
+
+            if key in ('id', 'interval', 'accuracy'):
+                w = QComboBox()
+                w.setObjectName(key)
+                w.setFixedWidth(60)
+                w.addItems(items)
+                w.currentTextChanged['QString'].connect(_update_data)
+            else:
+                if key in ['depth']:
+                    w = QDoubleSpinBox()
+                    w.setDecimals(1)
+                    w.setSingleStep(0.1)
+                else:
+                    w = QSpinBox()
+                    w.setSingleStep(1)
+                w.setObjectName(key)
+                w.setRange(*items)
+                w.setFixedWidth(60)
+                w.valueChanged.connect(_update_data)
+
+            layout.addWidget(QLabel(f"{name.capitalize()}:"), row, 0)
+            layout.addWidget(w, row, 1)
+            row += 1
+
+        _update_data()
+
+        return wgt
+
+    def createStatusbar(self):
+        '''
+        for (key, text) in (
+                ['tx', 'сообщений'],
+        ):
+            wgt = QLabel(' {}: {}'.format(text, 0))
+            wgt.setFixedWidth(90)
+            stretch = 2 if key == 'er' else 0
+            self.statusBar().addPermanentWidget(wgt, stretch)
+            self.status[key] = wgt
+        '''
+        pix = QLabel()
+        self.statusBar().addPermanentWidget(pix)
+        self.status['pixmap'] = pix
+        self.updatePixmap('noconnect')
+
+    def _on_start(self):
+        self._lock(True)
+
+        stg = self.read_settings()[0]
+        self.transmitter.configure(stg)
+
+        interval_ms = int(self.message_data['interval'])
+        self.timer_id = self.startTimer(interval_ms, timerType=QtCore.Qt.PreciseTimer)
+
+    def _on_stop(self):
+        if self.timer_id:
+            self.killTimer(self.timer_id)
+            self.timer_id = 0
+        self._lock(False)
+        self.updatePixmap('idle')
+
+    def _on_quit(self):
+        if self.timer_id:
+            self.killTimer(self.timer_id)
+        QtCore.QCoreApplication.exit(0)
+
+    def closeEvent(self, event):
+        self._on_quit()
+
+    def timerEvent(self, event):
+        data = self.read_settings()[1]
+
+        message = model.SonarMessage(self.read_settings()[1])
+        print(message)
+
+        msg_bytes = message.to_bytes()
+        print(msg_bytes)
+        self.transmitter.send(msg_bytes)
+
+        msg_ascii = message.to_ascii()
+        self.terminal.append(msg_ascii)
+
+        self.blinkPixmap()
+
+    def _lock(self, is_lock):
+        self.portbox.setDisabled(is_lock)
+        self.buttons['start'].setDisabled(is_lock)
+        self.buttons['stop'].setEnabled(is_lock)
+
+    def read_settings(self):
+        return self.portbox_data, self.message_data
+
+    def blinkPixmap(self):
+        if self.isBlink:
+            self.updatePixmap('tx')
+            self.isBlink = False
+        else:
+            self.updatePixmap('rx')
+            self.isBlink = True
+
+    def updatePixmap(self, state=None):
+        if not state:
+            state = "noconnect"
+        pixmaps = {
+            'noconnect': {'ico': ":/rc/network-offline.png", 'description': 'нет подключения'},
+            'idle': {'ico': ":/rc/network-idle.png", 'description': 'ожидание'},
+            'rx': {'ico': ":/rc/network-receive.png", 'description': 'прием'},
+            'tx': {'ico': ":/rc/network-transmit.png", 'description': 'передача'},
+            'error': {'ico': ":/rc/network-error.png", 'description': 'ошибка'}
+        }
+        self.status['pixmap'].setPixmap(QPixmap(pixmaps[state]['ico']))
+        self.status['pixmap'].setToolTip(pixmaps[state]['description'])
+
+    def updateStatus(self, key, value):
+        self.status[key].setText(' {}: {}'.format('отп', value))
+
+
+class UserialMainWindow(Ui):
+    pass
+
+
+if __name__ == '__main__':
+    
+    #with open(os.path.join(PROJECT_DIR, "degaus.json")) as json_file:
+    #   cfg = json.load(json_file)
+
+    available_ports = model.serial_ports()
+
+    app = QApplication(sys.argv)
+
+    # Add icon in the taskbar (only windows))
+    if sys.platform == 'win32':
+        import ctypes
+        myappid = u'navi-dals.kf1-m.udegausswer.001'  # arbitrary string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+        app.setWindowIcon(QIcon(':/rc/Interdit.ico'))
+
+    ex = UserialMainWindow()
+
+
+    sys.exit(app.exec_())
---
+++ model.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+
+import sys
+import glob
+
+import serial
+import serial.tools.list_ports as tools
+
+
+def serial_ports():
+    """ Lists serial port names
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+def find_ports():
+    return [info.device for info in tools.comports()]
+
+
+class Message:
+    START = '$'
+    END = '\r\n'
+
+    def __repr__(self):
+        return "{} {}".format(self.__class__.__name__, str(self.__dict__['fields']))
+
+    def checksum(self, data):
+        return ',*23'
+
+
+class SonarMessage(Message):
+    def __init__(self, data):
+        self.fields = dict.fromkeys(['id',
+                                     'field1', 'field2',
+                                     'depth', 'depth unit',
+                                     'danger', 'danger unit',
+                                     'accuracy'], '')
+
+        # Delete unnecessary key from data dictionary
+        self._data = {key:data[key] for key in (data.keys() & self.fields.keys())}
+
+        self._create(self._data)
+
+    def to_bytes(self):
+        return b''.join([char.encode() for char in self._message])
+
+    def to_ascii(self, without_end=True):
+        return self._message.rstrip('\r\n') if without_end else self._message
+
+    def _create(self, param):
+        data = param.copy()
+
+        data['depth'] = str(round(data['depth'], 2)).zfill(6)
+        data['danger'] = str(round(data['danger'], 2)).zfill(2)
+        data['depth unit'] = 'M'
+        data['danger unit'] = 'M'
+
+        self.fields.update(data)
+
+        self._message = self.START + ','.join(self.fields.values()) + self.checksum(data) + self.END
+
+
+class Transmitter:
+    def configure(self, settings):
+        self._settings = settings
+
+    def send(self, message: bytes):
+        with serial.Serial(port=self._settings['port'],
+                           baudrate=self._settings['baudrate'],
+                           timeout=0.1) as serobj:
+            serobj.write(message)
+
